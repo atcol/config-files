83f2201d54ff5e0b3e70e6e15baffc8a1d643c38
Alex <atc@localhost>
2024-08-04 21:10:11 +0100

    fix: use default nixos module

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
   outputs = inputs@{ nixpkgs, home-manager, flake-utils, ... }: 
     flake-utils.lib.eachDefaultSystem (system: {
-      nixosModules.home = { ... }: { options = {}; config = import ./home.nix; };
+      nixosModules.default = { ... }: { options = {}; config = import ./home.nix; };
     })
   ;
 }
