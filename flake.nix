2535936fc78071cbd1a5cf8960c6967cb147ca86
Alex <atc@localhost>
2024-08-04 20:44:49 +0100

    refactor: fix desc; export nixosModule

--- flake.nix
+++ flake.nix
@@ -1,29 +1,16 @@
 {
-  description = "NixOS configuration";
+  description = "My home-manager flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, ... }: {
-    nixosConfigurations = {
-      aelfred = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.atc = import ./home.nix;
-
-            # Optionally, use home-manager.extraSpecialArgs to pass
-            # arguments to home.nix
-          }
-        ];
-      };
-    };
-  };
+  outputs = inputs@{ nixpkgs, home-manager, flake-utils, ... }: 
+    flake-utils.lib.eachDefaultSystem (system: {
+      nixosModules."default-home" = { config, ... }: { options = {}; config = import ./home.nix; };
+    })
+  ;
 }
