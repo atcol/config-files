7fed189edf7ebc95b0f40da41adad37cfb732bc6
Alex <atc@localhost>
2025-12-06 16:11:34 +0000

    feat: more keybinds

--- vim/rustaceanvim.lua
+++ vim/rustaceanvim.lua
@@ -54,6 +54,36 @@ vim.g.rustaceanvim = {
         end,
         { silent = true, buffer = bufnr, desc = "Testables" }
       )
+
+      -- Explain error
+      vim.keymap.set(
+        "n",
+        "<leader>re",
+        function()
+          vim.cmd.RustLsp('explainError')
+        end,
+        { silent = true, buffer = bufnr, desc = "Explain Error" }
+      )
+
+      -- Quick documentation (Ctrl+Q in IntelliJ)
+      vim.keymap.set("n", "K", function()
+        vim.cmd.RustLsp({'hover', 'actions'})
+      end, { silent = true, buffer = bufnr, desc = "Hover Actions" })
+
+      -- Go to definition (Ctrl+B in IntelliJ)
+      vim.keymap.set("n", "gd", function()
+        vim.lsp.buf.definition()
+      end, { silent = true, buffer = bufnr, desc = "Go to Definition" })
+
+      -- Go to implementation (Ctrl+Alt+B in IntelliJ)
+      vim.keymap.set("n", "gi", function()
+        vim.lsp.buf.implementation()
+      end, { silent = true, buffer = bufnr, desc = "Go to Implementation" })
+
+      -- Find usages (Alt+F7 in IntelliJ)
+      vim.keymap.set("n", "gr", function()
+        vim.lsp.buf.references()
+      end, { silent = true, buffer = bufnr, desc = "Find References" })
     end,
   },
 }
