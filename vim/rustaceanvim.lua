700783cf635f81bf9c8433461644409d8aa71c83
Alex <atc@localhost>
2025-12-06 14:17:34 +0000

    feat: externalise lua config

---
+++ vim/rustaceanvim.lua
@@ -0,0 +1,48 @@
+-- Set leader key first if not already set
+vim.g.mapleader = "\\"
+
+vim.g.rustaceanvim = {
+  server = {
+    on_attach = function(client, bufnr)
+        -- Code actions (supports rust-analyzer's grouping)
+        vim.keymap.set(
+          "n", 
+          "<F5>", 
+          function()
+            vim.cmd.RustLsp('codeAction')
+          end,
+          { silent = true, buffer = bufnr, desc = "Code Action" }
+        )
+
+        -- Hover actions (override K)
+        vim.keymap.set(
+          "n", 
+          "<leader>ca",
+          function()
+            vim.cmd.RustLsp({'hover', 'actions'})
+          end,
+          { silent = true, buffer = bufnr, desc = "Hover Actions" }
+        )
+
+        -- Run/Debug
+        vim.keymap.set(
+          "n",
+          "<leader>rr",
+          function()
+            vim.cmd.RustLsp('runnables')
+          end,
+          { silent = true, buffer = bufnr, desc = "Runnables" }
+        )
+
+        -- Testing
+        vim.keymap.set(
+          "n",
+          "<leader>rt",
+          function()
+            vim.cmd.RustLsp('testables')
+          end,
+          { silent = true, buffer = bufnr, desc = "Testables" }
+        )
+    end,
+  },
+}
