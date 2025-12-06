7bc74927319fdff783bd5a84eebf037cb6e5cef4
Alex <atc@localhost>
2025-12-06 14:22:45 +0000

    feat: more keybinds

--- vim/rustaceanvim.lua
+++ vim/rustaceanvim.lua
@@ -4,45 +4,56 @@ vim.g.mapleader = "\\"
 vim.g.rustaceanvim = {
   server = {
     on_attach = function(client, bufnr)
-        -- Code actions (supports rust-analyzer's grouping)
-        vim.keymap.set(
-          "n", 
-          "<F5>", 
-          function()
-            vim.cmd.RustLsp('codeAction')
-          end,
-          { silent = true, buffer = bufnr, desc = "Code Action" }
-        )
 
-        -- Hover actions (override K)
-        vim.keymap.set(
-          "n", 
-          "<leader>ca",
-          function()
-            vim.cmd.RustLsp({'hover', 'actions'})
-          end,
-          { silent = true, buffer = bufnr, desc = "Hover Actions" }
-        )
+      -- Code actions (supports rust-analyzer's grouping)
 
-        -- Run/Debug
-        vim.keymap.set(
-          "n",
-          "<leader>rr",
-          function()
-            vim.cmd.RustLsp('runnables')
-          end,
-          { silent = true, buffer = bufnr, desc = "Runnables" }
-        )
+      vim.keymap.set(
+        "n",
+        "<F4>",
+        function()
+          vim.cmd.RustFmt
+        end,
+        { silent = true, buffer = bufnr, desc = "Rust format" }
+      )
 
-        -- Testing
-        vim.keymap.set(
-          "n",
-          "<leader>rt",
-          function()
-            vim.cmd.RustLsp('testables')
-          end,
-          { silent = true, buffer = bufnr, desc = "Testables" }
-        )
+      vim.keymap.set(
+        "n", 
+        "<F5>", 
+        function()
+          vim.cmd.RustLsp('codeAction')
+        end,
+        { silent = true, buffer = bufnr, desc = "Code Action" }
+      )
+
+      -- Hover actions (override K)
+      vim.keymap.set(
+        "n", 
+        "<leader>ca",
+        function()
+          vim.cmd.RustLsp({'hover', 'actions'})
+        end,
+        { silent = true, buffer = bufnr, desc = "Hover Actions" }
+      )
+
+      -- Run/Debug
+      vim.keymap.set(
+        "n",
+        "<leader>rr",
+        function()
+          vim.cmd.RustLsp('runnables')
+        end,
+        { silent = true, buffer = bufnr, desc = "Runnables" }
+      )
+
+      -- Testing
+      vim.keymap.set(
+        "n",
+        "<leader>rt",
+        function()
+          vim.cmd.RustLsp('testables')
+        end,
+        { silent = true, buffer = bufnr, desc = "Testables" }
+      )
     end,
   },
 }
