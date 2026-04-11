a35507cf8010dcb609cd807ff8c84b7e9059b702
Alex <atc@localhost>
2026-04-11 13:47:31 +0100

    feat: add kanagawa nvim colorscheme

--- vim/packer.lua
+++ vim/packer.lua
@@ -2,6 +2,7 @@ local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
   use 'sainnhe/sonokai'
+  use 'rebelot/kanagawa.nvim'
   -- use 'github/copilot.vim'  -- Disabled: conflicts with rust-analyzer
 
 end)
